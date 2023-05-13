198d4f9884c3b037f2b90f02a1f697ce8a3dd07b
CaioFerAmorim <caioferamorim@gmail.com>
2023-05-13 17:45:36 -0300

    apresentation section

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 <html lang="ptbr">
 <?php include_once './componnets/shared/Head/index.php'; ?>
 <body>
-    <main>
-
-    </main>
+<?php include_once './componnets/Appresentation/index.php';?>
 </body>
 </html>
